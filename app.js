6ce13fa2197abb9fb148281d167f5433f07bd0e3
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-12-14 00:42:01 +0000

    version2.0

---
+++ app.js
@@ -0,0 +1,7 @@
+const wmf = document.querySelector("#book-list li:nth-child(2) .name");
+
+var books = document.querySelectorAll("#book-list li .name");
+
+Array.from(books).forEach(function(book){
+    console.log(book);
+})
